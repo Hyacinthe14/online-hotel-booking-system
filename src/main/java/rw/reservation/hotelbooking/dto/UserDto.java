baadb1c6a82798034f90666f0632c2d3fb878dd8
Hyacinthe14 <hyacinthekwizerandiho@gmail.com>
2024-12-02 19:33:41 +0200

    update

--- src/main/java/rw/reservation/hotelbooking/dto/UserDto.java
+++ src/main/java/rw/reservation/hotelbooking/dto/UserDto.java
@@ -4,9 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+// import lombok.*;
+import rw.reservation.hotelbooking.entity.User;
 
 import java.time.LocalDate;
 
@@ -14,9 +17,11 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDto {
 
     private Long id;
+
     @NotEmpty(message = "First name should not be empty")
     private String firstName;
     @NotEmpty(message = "Last name should not be empty")
@@ -30,4 +35,15 @@ public class UserDto {
     private String phoneNumber;
     @Past(message = "Date of birth should be in the past")
     private LocalDate dateOfBirth;
+    public static UserDto fromUser(User user){
+    return UserDto.builder()
+    .id(user.getId())
+    .firstName(user.getFirstName())
+    .lastName(user.getLastName())
+    .email(user.getEmail())
+    .password(user.getPassword())
+    .phoneNumber(user.getPhoneNumber())
+    .dateOfBirth(user.getDateOfBirth())
+    .build();
+    }
 }
